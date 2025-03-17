64c0d64f007bfca22ae6cad3c8249eefe78f0288
Saurabh Mishra <84716420+Saurabhmishra1909@users.noreply.github.com>
2025-03-17 23:13:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this.name=name;
+		this.age=age;
+	}
+	set name(){
+		this._name=name;
+	}
+	get name(){
+		return this._name;
+	}
+	set age(){
+		this._age=age;
+	}
+	get age(){
+		return this._age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study(){
+		console.log(this.name+" is studying");
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach(){
+		console.log(this.name+" is teaching");
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
